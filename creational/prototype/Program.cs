c1e2d289949d6b05a798a0acb2e48668261e993c
Drakson58 <darksonbezerra123@gmail.com>
2021-11-04 00:38:57 -0300

    Prototype done

--- creational/prototype/Program.cs
+++ creational/prototype/Program.cs
@@ -2,6 +2,24 @@
 
 namespace prototype
 {
+    public class Person
+    {
+       public string Name { get; set; }
+       public int Age { get; set; }
+
+       public Person ShallowCopy()
+       {
+           return (Person)this.MemberwiseClone();
+       }
+
+        [Obsolete]
+        public Person DeepCopy()
+       {
+           Person person = (Person)this.MemberwiseClone();
+           person.Name = String.Copy(this.Name);
+           return person;
+       }
+    }
     class Program
     {
         static void Main(string[] args)
